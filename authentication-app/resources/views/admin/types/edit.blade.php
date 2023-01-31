92c35380470021bc550d8659f2091b75c197d38c
robpap96 <robertopapagni1996@outlook.it>
2023-01-31 10:46:10 +0100

    fix edit

---
+++ authentication-app/resources/views/admin/types/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h1>Aggiungi una nuova categoria</h1>
+
+    <form action="{{ route('admin.type.update') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+
+        <div class="mb-3">
+            <label for="type_id">Tipo</label>
+            <select class="form-select" name="type_id" id="type_id">
+                <option value="">Nessun Tipo</option>
+                @foreach ($types as $type)
+                    <option value="{{$type->id}}" {{ old('type_id') == $type->id ? 'selected' : '' }} >{{$type->project_type}}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <button type="submit" class="btn btn-success">Aggiungi</button>
+    </form>
+</div>
+@endsection
