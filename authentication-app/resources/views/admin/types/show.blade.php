7f3138eb0dc3786696a279fd58f91abaffa82fa2
robpap96 <robertopapagni1996@outlook.it>
2023-02-01 16:05:39 +0100

    aggiunta show

--- authentication-app/resources/views/admin/types/show.blade.php
+++ authentication-app/resources/views/admin/types/show.blade.php
@@ -1,11 +1,11 @@
-{{-- @extends('layouts.admin')
+@extends('layouts.admin')
 
 @section('content')
     <div class="container">
         <div class="py-4">
             <h2><a href="{{route('admin.types.index')}}">Torna alla lista</a></h2>
-            <h1>{{$type->project_type}}</h1>
-            <h3>Categoria: <a href="{{route('admin.types.show', $type)}}">{{$type->project_type}}</a></h3>
+            <h1>{{$type->name}}</h1>
+            <h3>Categoria: <a href="{{route('admin.types.show', $type)}}">{{$type->name}}</a></h3>
         </div>
     </div>
-@endsection --}}
+@endsection
