c1341d0b607a07bfc59e7209d7012a0eb87cb3e4
robpap96 <robertopapagni1996@outlook.it>
2023-02-01 16:21:47 +0100

    finita CRUD type

--- authentication-app/app/Http/Controllers/Admin/TypeController.php
+++ authentication-app/app/Http/Controllers/Admin/TypeController.php
@@ -103,6 +103,8 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $old_name = $type->name;
+        $type->delete();
+        return redirect()->route('admin.types.index')->with('message', "Il progetto $old_name è stato cancellato con successo!");
     }
 }
